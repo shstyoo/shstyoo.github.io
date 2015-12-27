825d2df3062d3e0adbcdd76f63e2d7f5cb586dd9
U-SPH35BA\shstyoo <SPH35BA+shstyoo@sph35ba.ad.fiu.edu>
2015-12-27 00:42:18 -0500

    started backbone for index.html, style.css, main.js

---
+++ main.js
@@ -0,0 +1,38 @@
+var main = function() {
+	
+	// Menu Sliding Toggle with buffer (to prevent multiple clicks)
+	$(".menuIconOpen").click(function(){
+		if( !$(".menuIconOpen").hasClass("pending") ) {
+			$(".menuIconOpen").addClass("pending");
+			$(".menu").animate({ left: "0px" }, 200);
+			setTimeout(removeOpenClass, 400);
+			//$(".menuIconOpen").fadeOut(200);
+			$(".menuIconOpen").hide();
+			//$(".menuIconClose").fadeIn(200);
+			$(".menuIconClose").show();
+			
+		}
+	});
+	$(".menuIconClose").click(function() {
+		if( !$(".menuIconClose").hasClass("pending") ){
+			$(".menuIconClose").addClass("pending");
+			$(".menu").animate({ left: "-155px" }, 200);
+			setTimeout(removeCloseClass, 400);
+			//$(".menuIconClose").fadeOut(200);
+			$(".menuIconClose").hide();
+			//$(".menuIconOpen").fadeIn(200);
+			$(".menuIconOpen").show();
+		}
+	});
+	
+	// setTimer Functions to remove class {pending} from the icons
+	var removeOpenClass = function() {
+		$(".menuIconOpen").removeClass("pending");
+	}
+	var removeCloseClass = function() {
+		$(".menuIconClose").removeClass("pending");
+	}
+	
+}
+
+$(document).ready(main);
