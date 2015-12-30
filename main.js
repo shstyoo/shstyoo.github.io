82cc32a6f873b02e4433b32a7065bc04590a1516
shstyoo <shstyoo@gmail.com>
2015-12-30 18:06:53 -0500

    testing populate 9

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ var main = function() {
 
 	// AJAX TESTING
 	// Function to populate repo names
-	/*
+	
 	function populateName(url) {
 		// Create new XMLHTTPRequest the repo names
 		var xhr = new XMLHttpRequest();
@@ -47,8 +47,9 @@ var main = function() {
 		xhr.send();
 	}
 	// Run function to populate repo name
-	populateName('/pyScripts/Outputh3.txt');*/
+	populateName('/pyScripts/Outputh3.txt');
 
+	/*
 	// Function to populate repo descriptions
 	// Same skeleton as populateName
 	function populateDesc(url) {
