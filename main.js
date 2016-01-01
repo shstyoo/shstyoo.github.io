12de75d25951bbef4be29c96884d5ded2d8b01d3
shstyoo <shstyoo@gmail.com>
2016-01-01 16:01:05 -0500

    debugging populate 3

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ var main = function() {
 			$('#projName').text(nameStringArray[0]);
 			// Get number of elements
 			numOfBullets = nameStringArray.length - 1;
-			$(".slider-dots").append("<li>" + numOfBullets + "</li>")
+			$(".slider-dots").append("<li>" + nameStringArray.length -1; + "</li>")
 		};
 		// Send request to server
 		xhr.open('GET', url, true);
