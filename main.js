f5ab0e09ad565e4b146569ed42f237c596acd68c
shstyoo <shstyoo@gmail.com>
2016-01-10 07:54:21 -0500

    testing HTML template using .clone()

--- main.js
+++ main.js
@@ -118,7 +118,7 @@ var main = function() {
 		$('.project-slides').clone().append($('.slide-template').html());
 		$('.slide-template').addClass('slide');
 		$('.slide-template').removeClass('slide-template');
-		alert('hiding class 1');
+		alert('hiding class 2');
 		$('.slide').find('.project-slide').addClass('1');
 		alert('showing class');
 	}
