47ca3b7be95e6948a5cdff2ce012360f0335b4c2
Steve Yoo <stshryu@gmail.com>
2017-11-10 22:05:01 -0500

    Updated JS

--- main.js
+++ main.js
@@ -1,37 +1,5 @@
 var main = function() {
-    alert('test');
-    // Menu Sliding Toggle with buffer (to prevent multiple clicks)
-    $(".menuIconOpen").click(function(){
-        if( !$(".menuIconOpen").hasClass("pending") ) {
-			$(".menuIconOpen").addClass("pending");
-			$(".menu").animate({ left: "45px" }, 200);
-			setTimeout(removeOpenClass, 400);
-			//$(".menuIconOpen").fadeOut(200);
-			$(".menuIconOpen").hide();
-			//$(".menuIconClose").fadeIn(200);
-			$(".menuIconClose").show();
-        }
-    });
-    $(".menuIconClose").click(function() {
-		if( !$(".menuIconClose").hasClass("pending") ){
-			$(".menuIconClose").addClass("pending");
-			$(".menu").animate({ left: "-870px" }, 200);
-			setTimeout(removeCloseClass, 400);
-			//$(".menuIconClose").fadeOut(200);
-			$(".menuIconClose").hide();
-			//$(".menuIconOpen").fadeIn(200);
-			$(".menuIconOpen").show();
-		}
-	});
-
-    // setTimer functions to remove class {pending} from icons
-    var removeOpenClass = function() {
-		$(".menuIconOpen").removeClass("pending");
-	}
-	var removeCloseClass = function() {
-		$(".menuIconClose").removeClass("pending");
-	}
-
+    alert('help');
 }
 
 $(document).ready(main);
